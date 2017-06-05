de7d0819d58f0e36422b2f26ebaab5938da6e356
Unknown <someguy2191@aim.com>
2017-06-05 16:38:13 -0400

    Bunch of stuff

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -9,14 +9,28 @@ using UnityEngine;
  */
 public class Player : Entity {
 
-	public int speed = 1;
+	public float speed = .5f;
+	private bool facingLeft = false;
+	private Rigidbody2D rigid2D;
+	private Animator anim;
+
 	//public int Health { get; set; }
 
-	public Player(int id, string name) : base(id, name) { }
+	// XXX: Are constructor methods even fucking used in unity?
+	// When is this even initailized? Forced to use Start() function instead.
+	public Player(int id, string name) : base(id, name) { 
+		//rigid2D = GetComponent<Rigidbody>();
+		//anim = GetComponent<Animator>();
+	}
+
+	public void Start() {
+		rigid2D = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
 
 	// Called whenever the fuck we want.
 	public override void doLogic() {
-		doMovement(true);
+		//doMovement(true);
 	}
 
 	/**
@@ -27,12 +41,32 @@ public class Player : Entity {
 	private void doMovement(bool canMove) {
 		if (!canMove) return;
 
-		float vert = getAxes("Vertical");
-		float hori = getAxes("Horizontal") ;
-		transform.Translate((hori * speed), (vert * speed), 0); // x, y, z
+		//float vert = getAxes("Vertical");
+		float rawHorizontal = getAxes("Horizontal") ;
+		rigid2D.velocity = new Vector2(rawHorizontal * speed, rigid2D.velocity.y);
+		anim.SetFloat("speed", Mathf.Abs(rawHorizontal));
+		if (rawHorizontal < 0 && !facingLeft) {
+			flipDirection();
+		} else if (rawHorizontal > 0 && facingLeft) {
+			flipDirection();
+		}
+
+
+		//transform.Translate((hori * speed), (vert * speed), 0); // x, y, z
+	}
+
+	private void flipDirection() {
+		facingLeft = !facingLeft;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
 	}
 
 	private float getAxes(string axes) {
 		return Input.GetAxis(axes);
+	}
+
+	public void FixedUpdate() {
+		doMovement(true);
 	}
 }
