0e86b36c25506bb7a45f2876e70f6e0f709249e7
Unknown <someguy2191@aim.com>
2017-06-06 08:25:10 -0400

    Removed depreciated assets, added simple mouse control.

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -12,8 +12,10 @@ public class Player : Entity {
 	// XXX: Public variables show in the inspector.
 	public float speed = 2f;
 	public float jumpForce = 75f;
+	public float mouseSensitivity = 1f;
 	public Transform checkGround;
 	public LayerMask whatIsGround;
+
 
 	// XXX: Private variables do not.
 	private bool facingLeft = false;
@@ -39,6 +41,13 @@ public class Player : Entity {
 	public override void doMovement(bool canMove) {
 		if (!canMove) return;
 		
+		float mouseXPosition = (Input.GetAxis("Mouse X") * mouseSensitivity);
+		//float mouseYPosition = (Input.GetAxis("Mouse Y") * mouseSensitivity);
+		doFlip(mouseXPosition);
+		//transform.Rotate(mouseXPosition, mouseYPosition, 0);
+		//verticalRotation = Mathf.Clamp(verticalRotation, -vertRotRange, vertRotRange);
+		//Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
 		if (onGround && (getAxes("Jump") > 0)) {
 			anim.SetBool("ground", false);
 			rigid2D.AddForce(new Vector2(0, jumpForce));
@@ -52,14 +61,16 @@ public class Player : Entity {
 		float rawHorizontal = getAxes("Horizontal") ;
 		rigid2D.velocity = new Vector2(rawHorizontal * speed, rigid2D.velocity.y);
 		anim.SetFloat("speed", Mathf.Abs(rawHorizontal));
-		if (rawHorizontal < 0 && !facingLeft) {
+		doFlip(rawHorizontal);
+		//transform.Translate((hori * speed), (vert * speed), 0); // x, y, z
+	}
+
+	private void doFlip(float condition) {
+		if (condition < 0 && !facingLeft) {
 			flipDirection();
-		} else if (rawHorizontal > 0 && facingLeft) {
+		} else if (condition > 0 && facingLeft) {
 			flipDirection();
 		}
-
-
-		//transform.Translate((hori * speed), (vert * speed), 0); // x, y, z
 	}
 
 	private void flipDirection() {
